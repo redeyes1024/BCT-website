cfa78b4c6dbc4eda17dfe942bb0f45fe87152c41
andrewpa <andrew.papadopoli@gmail.com>
2014-12-12 17:55:22 -0500

    Work on script to ease deployment (cont'd 2)

--- bct_site/public_html/deployment/set_source_paths.py
+++ bct_site/public_html/deployment/set_source_paths.py
@@ -1,5 +1,6 @@
 import argparse
 import re
+import pprint
 
 parser = argparse.ArgumentParser()
 
@@ -13,19 +14,19 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-if (args.config_location == None):
+if args.config_location == None:
     config_location = "./source_path_config.txt"
 else:
     config_location = args.config_location
 
 with open(config_location) as config_file:
-    config_file_cts = config_file.read()
+    config_file_cts = config_file.read()       
 
-def parse_config(sub_cts, parse_depth_counter):
+def parse_config(sub_cts, parse_depth_counter, parent_prop_list):
 
-  ####  result_dict = {}
+    sub_result_dict = {}
 
-    regexp = "(^|\n)" + ("#" * parse_depth_counter) + "\w+"
+    regexp = "(^|\n|\s{4})" + ("#" * parse_depth_counter) + "\w+"
 
     labels = re.finditer(regexp, sub_cts)
 
@@ -40,17 +41,26 @@ def parse_config(sub_cts, parse_depth_counter):
     for label in labels:
         label_counter += 1
         cur_label = label.group()
-        cor_label = cur_label[1:] if cur_label[0] == '\n' else cur_label
+        cor_label = re.sub("[\n|#| ]", "", cur_label)
         label_list.append(cor_label)
         label_cts_idx_list.append(label.start())
         label_cts_idx_list.append(label.end())
-    print("\n", parse_depth_counter, "\n", sub_cts, regexp, "\n")
+
     if label_counter == 0:
+        parent_dict = result_dict
+        for parent_prop in parent_prop_list[:len(parent_prop_list)-1]:
+            cur_prop = parent_dict[parent_prop]
+            parent_dict = cur_prop
+
+        target_parent_prop = parent_prop_list[len(parent_prop_list)-1]
+        sub_cts_cor = re.sub("[\n|#| ]", "", sub_cts)
+        parent_dict[target_parent_prop] = sub_cts_cor
+
         return True
 
     for idx in range(0, len(label_cts_idx_list)):
-        if (idx % 2 != 0):
-            if (idx + 1 != len(label_cts_idx_list)):
+        if idx % 2 != 0:
+            if idx + 1 != len(label_cts_idx_list):
                 cur_idx_list = [
                     label_cts_idx_list[idx],
                     label_cts_idx_list[idx + 1]
@@ -60,20 +70,35 @@ def parse_config(sub_cts, parse_depth_counter):
                     label_cts_idx_list[idx],
                     len(sub_cts)
                 ]
+
             label_cts_idx_list_list.append(cur_idx_list)
 
     list_list_count = 0
 
     for list_list in label_cts_idx_list_list:
         cur_cts = sub_cts[list_list[0]:list_list[1]]
-        result_dict.update({
-            label_list[list_list_count]: cur_cts
-        });
+        sub_result_dict.update({ label_list[list_list_count]: cur_cts })
         list_list_count += 1
 
     parse_depth_counter += 1
 
-    for label, new_sub_cts in result_dict.items():
-        parse_config(new_sub_cts, parse_depth_counter)
+    if len(parent_prop_list) == 0:
+        global result_dict
+        result_dict = sub_result_dict
+    else:
+        parent_dict = result_dict
+        for parent_prop in parent_prop_list[:len(parent_prop_list)-1]:
+            cur_prop = parent_dict[parent_prop]
+            parent_dict = cur_prop
 
-parse_config(config_file_cts, 1)
+        target_parent_prop = parent_prop_list[len(parent_prop_list)-1]
+        parent_dict[target_parent_prop] = sub_result_dict
+
+    for label, new_sub_cts in sub_result_dict.items():
+        next_parent_prop_list = parent_prop_list[:]
+        next_parent_prop_list.append(label)
+        parse_config(new_sub_cts, parse_depth_counter, next_parent_prop_list)
+
+parse_config(config_file_cts, 1, [])
+
+pprint.pprint(result_dict)
